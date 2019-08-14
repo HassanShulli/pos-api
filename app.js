5e7753af3367510d2081d15ad9b3fda2091605a8
Hassan Shulli <hassan14x@hotmail.com>
2019-08-15 02:54:39 +0800

    deploying to heroku

--- app.js
+++ app.js
@@ -14,34 +14,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/POS_DB", {useMongoClient: true});  //for local mongo use
-// mongoose.connect("mongodb://pos_admin1:pos_admin1@ds151530.mlab.com:51530/pos_app", {useMongoClient: true}); //mlab
+// mongoose.connect("mongodb://localhost:27017/POS_DB", {useMongoClient: true});  //for local mongo use
+mongoose.connect("mongodb://pos_admin1:pos_admin1@ds151530.mlab.com:51530/pos_app", {useMongoClient: true}); //mlab
 mongoose.connection.on('error', function (err) {
     console.error(err);
     process.exit();
 });
-
-// app.use(expressJwt({
-//     secret: 'pos-app-shared-secret',
-//     getToken: function fromHeaderOrQuerystring(req) {
-//         if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-//             return req.headers.authorization.split(' ')[1];
-//         } else if (req.query && req.query.token) {
-//             return req.query.token;
-//         }
-//         return null;
-//     }
-// }).unless({
-//     path: [
-//         '/',
-//         '/user/login',
-//         '/user/register'
-//     ]
-// }), function (err, req, res, next) {
-//     if (err) {
-//         res.status(401).send({ success: false, result: [], messages: [err.message] })
-//     }
-// });
 
 // allow-cors
 app.use(function (req, res, next) {
@@ -60,7 +38,6 @@ app.use(function (req, res, next) {
 const table = require('./routers/table');
 const item = require('./routers/item');
 const order = require('./routers/order');
-const user = require('./routers/user');
 const counter = require('./routers/counter');
 
 //initialise express router
@@ -69,7 +46,6 @@ var router = express.Router();
 app.use("/table", table);
 app.use("/item", item);
 app.use("/order", order);
-app.use("/user", user);
 app.use("/counter", counter);
 
 // catch 404
